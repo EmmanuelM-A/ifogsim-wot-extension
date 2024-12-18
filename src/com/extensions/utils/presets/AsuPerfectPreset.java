680cc09e5ca91db2bf2009769112d5263e1fca3d
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-12-18 15:19:40 +0000

    Added generic configuration presets for the VD's fogDevice.

---
+++ src/com/extensions/utils/presets/AsuPerfectPreset.java
@@ -0,0 +1,15 @@
+package com.extensions.utils.presets;
+
+import org.fog.entities.FogDevice;
+
+/**
+ * This class represents the "ASU_PERFECT" configuration for FogDevices, assuming near-perfect performance with maximum
+ * available resources.
+ */
+public class AsuPerfectPreset extends FogDevicePreset {
+
+    @Override
+    public void apply(FogDevice fogDevice) {
+
+    }
+}
