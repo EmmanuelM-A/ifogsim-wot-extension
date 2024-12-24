da44c10adfe89562754c4ad45a9444ed28cc1a31
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2024-12-24 15:07:15 +0000

    Added a class that handles the reports for VDs and any issues that might be flagged during creation or validation.

---
+++ src/com/extensions/vdcreation/core/validation/ValidationReport.java
@@ -0,0 +1,13 @@
+package com.extensions.vdcreation.core.validation;
+
+/**
+ * Contains a report of all VD validators
+ * - Original List of VDs
+ * - List of VDs with VD that did not pass removed
+ * - List of VDs that did not pass and the areas they did not pass
+ * - Percentage of VDs that passed validation (define a threshold of acceptance)
+ * - Log issues and successes
+ * - Calculate acceptance
+ */
+public class ValidationReport {
+}
