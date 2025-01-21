16ecf1acf9317194a9697068daf55d4275a335d8
EmmanuelM-A <madukaagbeze.ea@gmail.com>
2025-01-21 22:17:28 +0000

    Added a centralized main class to run the program.

--- src/com/extensions/App.java
+++ src/com/extensions/App.java
@@ -1,40 +1,86 @@
 package com.extensions;
 
 import com.extensions.utils.FilePaths;
+import com.extensions.utils.presets.ActuatorPreset;
+import com.extensions.utils.presets.FogDevicePreset;
+import com.extensions.utils.presets.SensorPreset;
 import com.extensions.vdcreation.core.JsonFileProcessor;
 import com.extensions.vdcreation.core.VirtualDevice;
+import com.extensions.vdcreation.core.VirtualDeviceFactory;
 import com.extensions.vdcreation.models.ThingDescription;
 import com.extensions.vdcreation.parsers.ThingDescriptionParser;
+import com.extensions.vdcreation.parsers.VirtualDeviceConfigParser;
 import org.cloudbus.cloudsim.Log;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.fog.entities.FogBroker;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public final class App {
     public static void main(String[] args) {
-        /*try {
-            // Extract metadata from TDs
-            thingDescriptions = JsonFileProcessor.processJsonFiles(FilePaths.JSON_THINGS_REPO.getFilepath(), new ThingDescriptionParser());
+        // Determines if the application deployment is cloud-based.
+        boolean CLOUD = false;
 
-        } catch (IOException e) {
+        List<VirtualDevice> virtualDevices = new ArrayList<>();
+
+        Log.printLine("Starting Simulation...");
+
+        try {
+            Log.disable();
+
+            //////////////////////////////// INITIAL SETUP ////////////////////////////////
+
+            // Specifies the number of users interacting with the cloud.
+            int numUsers = 1;
+
+            // Initializes a calendar object to track simulation time and events.
+            Calendar calendar = Calendar.getInstance();
+
+            // Determines whether to enable tracing of simulation events for debugging purposes.
+            boolean traceFlag = false;
+
+            // Initializes the CloudSim toolkit with the specified number of users, the calendar instance, and trace settings.
+            CloudSim.init(numUsers, calendar, traceFlag);
+
+            // Assigns a unique identifier to the application being simulated. This ID is used to manage the application's components and operations.
+            String appId = "";
+
+            // Initializes a FogBroker, which manages application modules and coordinates communication between them in the simulation.
+            FogBroker broker = new FogBroker("broker");
+
+            //////////////////////////////// VIRTUAL DEVICE CREATION ////////////////////////////////
+
+            // Extract the metadata from the TDs
+            List<ThingDescription> thingDescriptions = JsonFileProcessor.processJsonFiles(
+                    "", // SET THINGS REPO HERE
+                    new ThingDescriptionParser()
+            );
+
+            // Set up the VD factory to create VDs with the appropriate presets
+            VirtualDeviceFactory virtualDeviceFactory = new VirtualDeviceFactory(broker.getId(), appId, FogDevicePreset.DEFAULT, SensorPreset.DEFAULT, ActuatorPreset.DEFAULT);
+            VirtualDeviceConfigParser vdConfigParser = new VirtualDeviceConfigParser();
+
+            // Create the virtual devices using the thing descriptions and factory method
+            for(ThingDescription thingDescription : thingDescriptions) {
+                VirtualDevice vd = virtualDeviceFactory.createVirtualDevice(
+                        thingDescription,
+                        vdConfigParser.process(new File("")) // SET VD'S CONFIG FILE HERE
+                );
+                // Validate VD HERE
+                virtualDevices.add(vd);
+            }
+
+            //////////////////////////////// APPLICATION SETUP ////////////////////////////////
+
+
+
+            //////////////////////////////// SIMULATION ////////////////////////////////
+        } catch(Exception e) {
             e.printStackTrace();
-        }*/
-
-        // User passed in documents
-            // A folder containing TDs
-            // JSON file of the nodeRED system design
-            // Config files for VDs (OPTIONAL)
-
-        // Load and create VDs from TDs
-
-        // Load NodeRED translated system design
-
-        // Construct system using system design and VDs
-            // Apply configs to tagged element if any
-
-        // Start simulation application
-
-        // Stop simulation
+        }
     }
 }
